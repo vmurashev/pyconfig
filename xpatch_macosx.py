2c7ef735d7f2fb5a0bdc30f70328aad939b69273
vmurashev <vitaly.murashev@gmail.com>
2018-06-28 10:18:02 +0300

    macosx patching, python3.7

--- xpatch_macosx.py
+++ xpatch_macosx.py
@@ -10,3 +10,8 @@ if __name__ == '__main__':
     py2_output_file = os.path.normpath(os.path.join(DIR_HERE, 'output/python-2.7/pyconfig_macosx.h'))
     py2_args = [sys.executable, PATCH_SCRIPT, '--abi', 'macosx', '--input', py2_input_file, '--output', py2_output_file]
     subprocess.check_call(py2_args)
+
+    py3_input_file = os.path.normpath(os.path.join(DIR_HERE,   'input/python-3.7/pyconfig_macosx.h'))
+    py3_output_file = os.path.normpath(os.path.join(DIR_HERE, 'output/python-3.7/pyconfig_macosx.h'))
+    py3_args = [sys.executable, PATCH_SCRIPT, '--abi', 'macosx', '--input', py3_input_file, '--output', py3_output_file]
+    subprocess.check_call(py3_args)
